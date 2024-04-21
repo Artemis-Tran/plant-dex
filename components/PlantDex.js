f6ef732160a25e07f43957d966e41730a42f7ebb
Artemis Tran <artemistranhb@gmail.com>
2024-04-21 02:34:44 -0700

    Implemented a gallery for Plant Dex

--- components/PlantDex.js
+++ components/PlantDex.js
@@ -1,14 +1,39 @@
 import { Text, Image, View, SafeAreaView, StyleSheet, ImageBackground, TouchableOpacity, FlatList, Alert } from 'react-native';
 import React from 'react';
 import { useFonts, InknutAntiqua_900Black} from '@expo-google-fonts/inknut-antiqua';
+
+import Gallery from "./Gallery"
 import Navbar from "./Navbar";
 
+const images = [
+    { source:require('../assets/images/plant.png') },
+    { source:require('../assets/images/plant.png') },
+    { source:require('../assets/images/plant.png') },
+    { source:require('../assets/images/plant.png') },
+    { source:require('../assets/images/plant.png') },
+    { source:require('../assets/images/plant.png') },
+  ];
+
 const PlantDex = ({navigation}) => {
-    
+
+let [fontsLoaded] = useFonts({
+    InknutAntiqua_900Black,
+    });
+
+
 return(
 <SafeAreaView style={styles.safeArea}>
-    <Text style={styles.title}>Hello World!</Text>
-    <Navbar navigation={navigation} />
+    <View style={styles.titleContainer}>
+        <Text style={styles.title}>Plant Dex</Text>
+    </View>
+    <View style={styles.container}>
+        <Gallery images={images}>
+
+        </Gallery>
+    </View>
+    <View style={styles.nav}> 
+        <Navbar navigation={navigation} />
+    </View>
 </SafeAreaView>
 );
 }
@@ -16,67 +41,26 @@ return(
 const styles = StyleSheet.create({
     safeArea: {
         flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: "#b9db92"
-        
-    },
-    title: {
-        alignItems: 'center',
-        justifyContent: 'center'
     },
     container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-      },
-      header: {
-        padding: 12,
-        borderWidth: 1,
-        borderColor: '#68FF42',
-        width: '100%',
-        alignItems: 'center',
-        backgroundColor: '#77dd77',
-      },
-      headerText: {
-        fontSize: 25,
-        fontFamily: 'InknutAntiqua_900Black',
-        fontWeight: 'bold',
-        padding: 8,
-        marginTop: 12,
-      },
-      header2Text: {
-        fontSize: 12,
-        fontFamily: 'InknutAntiqua_900Black',
-        fontWeight: 'bold',
-        padding: 6, 
-        textAlign: 'left',
-      },
-      header2Container: {
-        alignItems: 'flex-start', // Aligns children to the start (left)
-        width: '100%', // Ensures the container fills the width of its parent
-        paddingHorizontal: 10,
-      },
-    
-      grid: {
-        flex: 1,
-        alignSelf: 'stretch',
-      },
-      itemContainer: {
-        // Adjust your item container styling
         alignItems: 'center',
         justifyContent: 'center',
-        margin: 15,
-        height: 100, 
-      },
-      plantImage: {
-        width: 90, 
-        height: 90, 
-        marginTop: 10,
-      },
-      plantName: {
-        // Style for the plant name if needed
-      },
-})
+    },
+    titleContainer: {
+        backgroundColor: '#b9db92',
+        padding: 20,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontFamily: 'InknutAntiqua_900Black',
+        padding: 10,
+    },
+    nav: {
+        justifyContent: 'flex-end',
+        paddingTop: 15,
+    }
+
+});
+
 export default PlantDex;
